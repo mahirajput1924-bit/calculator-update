248c2a7442b3e711dbda8455c294c9464d1c0b7c
mahirajput1924-bit <mahirajput1924@gmail.com>
2026-01-08 17:52:52 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,13 @@
+n1 =float(input("enter number :"))
+n2 =float (input("enter number:"))
+op = input ("enter what you want to do(+,-,*,/,%):")
+if op == "+":
+    print (n1+n2)
+elif op == "-":
+ print (n1-n2)
+elif op == "*":
+ print (n1*n2)
+elif op == "/":
+ print (n1/n2)
+elif op == "%":
+  print (n1%n2)
